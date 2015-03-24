648712fd71e2cd202143ee811ed3b282d2878e07
samamaguire <smaguire6@me.com>
2015-03-24 17:28:19 -0400

    procfile

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: '5 POINT INTERACTIVE' });
 });
 
 module.exports = router;
+
+
